5364b26595ae73a56b78bd7851b5cef8c7003853
CarloCabico <carlocabico20@yahoo.com>
2014-08-12 18:07:15 +0800

    commit
    commit

---
+++ ChartProgram/ChartProgram/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace ChartProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.LineChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column12 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column13 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column14 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column15 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column17 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column18 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column19 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column20 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column21 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column22 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column23 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column24 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.LineChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(12, 492);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(866, 21);
+            this.btnPrint.TabIndex = 2;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            // 
+            // LineChart
+            // 
+            chartArea1.AxisX.IsMarksNextToAxis = false;
+            chartArea1.AxisX.LabelStyle.Format = "0:00";
+            chartArea1.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisX.ScaleView.SmallScrollSize = 1D;
+            chartArea1.AxisX.ScrollBar.Enabled = false;
+            chartArea1.AxisX2.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisY.Interval = 10D;
+            chartArea1.AxisY.LabelStyle.Format = "0°C";
+            chartArea1.AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisY.ScrollBar.Enabled = false;
+            chartArea1.AxisY2.Interval = 0.5D;
+            chartArea1.AxisY2.LabelStyle.Format = "P0";
+            chartArea1.AxisY2.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.AxisY2.ScrollBar.Enabled = false;
+            chartArea1.CursorX.IntervalOffset = 1D;
+            chartArea1.CursorX.SelectionStart = 1D;
+            chartArea1.Name = "ChartArea1";
+            this.LineChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.LineChart.Legends.Add(legend1);
+            this.LineChart.Location = new System.Drawing.Point(12, 12);
+            this.LineChart.Name = "LineChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            series1.Label = "#VALX{0:00}";
+            series1.Legend = "Legend1";
+            series1.Name = "Temperature";
+            series1.SmartLabelStyle.AllowOutsidePlotArea = System.Windows.Forms.DataVisualization.Charting.LabelOutsidePlotAreaStyle.Yes;
+            series1.SmartLabelStyle.MovingDirection = ((System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles)(((((((((System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.Top | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.Bottom) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.Right) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.Left) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.TopLeft) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.TopRight) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.BottomLeft) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.BottomRight) 
+            | System.Windows.Forms.DataVisualization.Charting.LabelAlignmentStyles.Center)));
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Legend = "Legend1";
+            series2.Name = "Humidity";
+            series2.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            this.LineChart.Series.Add(series1);
+            this.LineChart.Series.Add(series2);
+            this.LineChart.Size = new System.Drawing.Size(866, 300);
+            this.LineChart.TabIndex = 3;
+            this.LineChart.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10,
+            this.Column11,
+            this.Column12,
+            this.Column13,
+            this.Column14,
+            this.Column15,
+            this.Column16,
+            this.Column17,
+            this.Column18,
+            this.Column19,
+            this.Column20,
+            this.Column21,
+            this.Column22,
+            this.Column23,
+            this.Column24});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 318);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(760, 168);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "1";
+            this.Column1.Name = "Column1";
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "2";
+            this.Column2.Name = "Column2";
+            this.Column2.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "3";
+            this.Column3.Name = "Column3";
+            this.Column3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "4";
+            this.Column4.Name = "Column4";
+            this.Column4.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "5";
+            this.Column5.Name = "Column5";
+            this.Column5.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "6";
+            this.Column6.Name = "Column6";
+            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "7";
+            this.Column7.Name = "Column7";
+            this.Column7.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "8";
+            this.Column8.Name = "Column8";
+            this.Column8.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "9";
+            this.Column9.Name = "Column9";
+            this.Column9.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "10";
+            this.Column10.Name = "Column10";
+            this.Column10.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "11";
+            this.Column11.Name = "Column11";
+            this.Column11.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column12
+            // 
+            this.Column12.HeaderText = "12";
+            this.Column12.Name = "Column12";
+            this.Column12.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column13
+            // 
+            this.Column13.HeaderText = "13";
+            this.Column13.Name = "Column13";
+            this.Column13.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column14
+            // 
+            this.Column14.HeaderText = "14";
+            this.Column14.Name = "Column14";
+            this.Column14.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column15
+            // 
+            this.Column15.HeaderText = "15";
+            this.Column15.Name = "Column15";
+            this.Column15.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column16
+            // 
+            this.Column16.HeaderText = "16";
+            this.Column16.Name = "Column16";
+            this.Column16.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column17
+            // 
+            this.Column17.HeaderText = "17";
+            this.Column17.Name = "Column17";
+            this.Column17.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column18
+            // 
+            this.Column18.HeaderText = "18";
+            this.Column18.Name = "Column18";
+            this.Column18.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column19
+            // 
+            this.Column19.HeaderText = "19";
+            this.Column19.Name = "Column19";
+            this.Column19.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column20
+            // 
+            this.Column20.HeaderText = "20";
+            this.Column20.Name = "Column20";
+            this.Column20.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column21
+            // 
+            this.Column21.HeaderText = "21";
+            this.Column21.Name = "Column21";
+            this.Column21.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column22
+            // 
+            this.Column22.HeaderText = "22";
+            this.Column22.Name = "Column22";
+            this.Column22.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column23
+            // 
+            this.Column23.HeaderText = "23";
+            this.Column23.Name = "Column23";
+            this.Column23.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Column24
+            // 
+            this.Column24.HeaderText = "24";
+            this.Column24.Name = "Column24";
+            this.Column24.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(778, 449);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Update Graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 525);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.LineChart);
+            this.Controls.Add(this.btnPrint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.LineChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.DataVisualization.Charting.Chart LineChart;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column12;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column13;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column14;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column15;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column17;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column18;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column19;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column20;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column21;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column22;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column23;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column24;
+    }
+}
+
---
+++ ChartProgram/ChartProgram/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChartProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LineChart.ChartAreas[0].AxisX.Minimum = 100;
+            LineChart.ChartAreas[0].AxisY.Minimum = -30;
+            LineChart.ChartAreas[0].AxisY.Maximum = 30;
+            LineChart.ChartAreas[0].AxisY2.Minimum = 0;
+            LineChart.ChartAreas[0].AxisY2.Maximum = 1;
+           
+            this.dataGridView1.Rows.Add(60,60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60);
+            this.dataGridView1.Rows.Add(-20,-10,-20,-10,-30,-7,-8,-9,-10,-10,-10,-10,-4,-8,-9,-10,-9,-20,-10,-10,-19,-5,-3,-20);
+            this.dataGridView1.Rows.Add(80, 100, 70, 50, 60, 70, 80, 90, 100, 100, 100, 100, 70, 80, 90, 100, 90, 60, 100, 100, 70, 50, 85, 80);
+            
+            LineChart.DataBind();
+            FillData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LineChart.DataBind();
+            FillData();
+        }
+
+        private void FillData()
+        {
+            //LineChart.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            LineChart.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            LineChart.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+            LineChart.ChartAreas[0].AxisX.ScaleView.Zoom(0, 1500);
+            List<double> xvalues = new List<double>();
+            List<double> yvalues = new List<double>();
+            List<double> y2values = new List<double>();
+            List<double> xval2 = new List<double>();
+            //List<int> yval2 = new List<int>();
+            double counter = 0;
+            double counter2 = 1;
+            double count2 = 100;
+            double value = 0;
+            //double add = 0;
+            foreach (DataGridViewRow xval in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in xval.Cells)
+                {
+                    value = Convert.ToDouble(cell.Value.ToString());
+                    if(counter <= 23)
+                    {
+                        //if (value > 60)
+                        //{
+                        value = (value / 60) * count2;
+                        //}
+                        //if(counter == 0)
+                        //{
+                        //    xvalues.Add(1);
+                        //}
+                       xvalues.Add(value);        
+                       counter++;
+                       count2 += 100;
+
+                    }
+                    else if (counter > 23 && counter <= 47)
+                    {
+                        yvalues.Add(value);
+                       // LineChart.Series["Temperature"].Points.;
+                      //  LineChart.Series["Temperature"].YValueMembers = value.ToString();
+                       // LineChart.Series["Temperature"].Points.AddXY(0, value);
+                        counter++;
+                       // LineChart.DataSource = dataGridView1.DataSource;
+                        //LineChart.Update();
+                    }       
+                    else if (counter > 47)
+                    {
+                        value = value / 100;
+                        y2values.Add(value);
+                        counter++;
+                    }
+                    //label1.Text = value;
+                }
+
+               
+              // LineChart.Series["Temperature"].Points.AddY(0, yvalues);
+                
+              // LineChart.Series["Temperature"].Points.DataBindXY(xvalues, yvalues);
+                if(xvalues.Count > 0 && yvalues.Count > 0 && y2values.Count > 0)
+                {
+                   
+                    //xval2.Add(1);
+                    //xval2.Add(xvalues[0]);
+                    //xval2.Add(xvalues[1]);
+                    //xval2.Add(xvalues[2]);
+                    //xval2.Add(xvalues[3]);
+                    //xval2.Add(xvalues[4]);
+                    //xval2.Add(xvalues[5]);
+                    //xval2.Add(xvalues[6]);
+                    //xval2.Add(xvalues[7]);
+                    //xval2.Add(xvalues[8]);
+                    //xval2.Add(xvalues[9]);
+                    //xval2.Add(xvalues[10]);
+                    //xval2.Add(xvalues[11]);
+                    //xval2.Add(xvalues[12]);
+                    //xval2.Add(xvalues[13]);
+                    //xval2.Add(xvalues[14]);
+                    //xval2.Add(xvalues[15]);
+                    //xval2.Add(xvalues[16]);
+                    //xval2.Add(xvalues[17]);
+                    //xval2.Add(xvalues[18]);
+                    //xval2.Add(xvalues[19]);
+                    //xval2.Add(xvalues[20]);
+                    //xval2.Add(xvalues[21]);
+                    //xval2.Add(xvalues[22]);
+                    //xval2.Add(xvalues[23]);
+                    //xval2.Remove(xval2[24]);
+
+                    //LineChart.ChartAreas[0].AxisX.Interval = 2;   
+                    LineChart.Series["Temperature"].Points.DataBindXY(xvalues, yvalues);
+                    LineChart.Series["Humidity"].Points.DataBindXY(xvalues, y2values);
+                   
+                    //if (LineChart.ChartAreas[0].AxisX.Maximum == 10)
+                    //{
+                    //    //chart1.ChartAreas["Area1"].AxisY.ScrollBar.Size = 10;
+                    
+                    //    //get the current maximum of the axis
+                    //    double ymax = LineChart.ChartAreas[0].AxisY.Maximum;
+                    //    //Add the new data
+
+                    //    //set the zoom size to the exisiting scale values other wise nothing happens
+                    //    LineChart.ChartAreas[0].AxisY.ScaleView.Zoom(0, ymax);
+                    //}
+                    return;
+                }
+                
+           }
+            
+           // LineChart.Series["Temperature"].XValueMember = dataGridView1.Columns[0].DataPropertyName;
+          // LineChart.Series["Temperature"].YValueMembers = dataGridView1.Columns[1].DataPropertyName; //.DataPropertyName
+            //object check = dataGridView1.Rows[0].Cells;
+            //object check2 = dataGridView1.Rows[1].Cells;
+   
+            //
+           // LineChart.DataSource = dataGridView1.DataSource;
+            //LineChart.Update();         
+        }
+    }
+}
